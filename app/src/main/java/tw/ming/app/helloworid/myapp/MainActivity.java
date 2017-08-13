113b68a8d5206d9f1737e83c144f348668911cbe
LBJ <LBJ>
2017-08-13 13:45:36 +0800

    20170813

--- app/src/main/java/tw/ming/app/helloworid/myapp/MainActivity.java
+++ app/src/main/java/tw/ming/app/helloworid/myapp/MainActivity.java
@@ -1,7 +1,12 @@
 package tw.ming.app.helloworid.myapp;
 
+import android.Manifest;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,20 +28,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sp = getSharedPreferences("gamedata",MODE_PRIVATE);
-        editor = sp.edit();
-
-        sdroot = Environment.getExternalStorageDirectory();
-        approot = new File(sdroot,"Android/data/"+getPackageName()+"/");
-        if(!approot.exists()){
-            approot.mkdir();
+        //查看是否取得權限,若沒有則去取得權限,權限也存於暫存資料夾
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    123);
+        }else{
+            init();
         }
-
 //        String state = Environment.getExternalStorageState();//外存
 //        Log.i("ming",state);
 
         File sd = Environment.getExternalStorageDirectory();//外存目錄
         Log.i("ming",sd.getAbsolutePath());
+    }
+
+    private void init(){
+        sp = getSharedPreferences("gamedata", MODE_PRIVATE);    // gamedata.xml
+        editor = sp.edit();
+
+        sdroot = Environment.getExternalStorageDirectory();
+        approot = new File(sdroot, "Android/data/" + getPackageName() + "/");
+        if (!approot.exists()){
+            approot.mkdirs();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 123){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                init();
+            }else{
+                finish();
+            }
+        }
     }
 
     public void test1(View view){
@@ -56,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
         {
             fout.write("Hello\nHello,Ming\n1234567\n".getBytes());
             fout.flush();
-            Toast.makeText(this,"Save OK",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,"Save OK",Toast.LENGTH_SHORT).show();//放置暫存資料夾
         }catch (Exception e){
             Log.i("ming",e.toString());
         }
@@ -70,6 +101,32 @@ public class MainActivity extends AppCompatActivity {
             }
             Toast.makeText(this,sb.toString(),Toast.LENGTH_SHORT).show();
         }catch (Exception e){
+            Log.i("ming",e.toString());
+        }
+    }
+
+    public void test5(View view){
+        File file = new File(sdroot,"file.txt");
+        try{
+        FileOutputStream fout = new FileOutputStream(file);
+        fout.write("ok".getBytes());
+        fout.flush();
+            fout.close();
+        }
+        catch (Exception e){
+            Log.i("ming",e.toString());
+        }
+    }
+
+    public void test6(View view){
+        File file = new File(approot,"file.txt");
+        try{
+            FileOutputStream fout = new FileOutputStream(file);
+            fout.write("ok".getBytes());
+            fout.flush();
+            fout.close();
+        }
+        catch (Exception e){
             Log.i("ming",e.toString());
         }
     }
